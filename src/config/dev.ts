9cd5bf1ed57b405a7d354e4d648a75791aed8dfa
Christian Lisangola <christian.lisangola@gmail.com>
2020-03-30 16:45:48 +0100

    Removed buil js files

--- src/config/dev.ts
+++ src/config/dev.ts
@@ -1,6 +1,6 @@
 export const DBInfos = {
-  MONGO_USER: 'fakenews123',
-  MONGO_PASSWORD: 'fakenews123',
-  MONGO_DBNAME: 'fake_new_plateforme_dev',
-  MONGO_HOSTNAME: 'ds233198.mlab.com:33198'
+  MONGO_USER: '',
+  MONGO_PASSWORD: '',
+  MONGO_DBNAME: '',
+  MONGO_HOSTNAME: '.mlab.com:33198'
 };
