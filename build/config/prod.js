f725bdaf16126cd9e4e110776ac8f695c0e894d5
Christian Lisangola <christian.lisangola@gmail.com>
2020-03-31 09:43:08 +0100

    Fixed ignored dotenv config

--- build/config/prod.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.DBInfos = {
-    MONGO_USER: process.env.MONGO_USER,
-    MONGO_PASSWORD: process.env.MONGO_PASSWORD,
-    MONGO_DBNAME: process.env.MONGO_DBNAME,
-    MONGO_HOSTNAME: process.env.MONGO_HOSTNAME
-};
