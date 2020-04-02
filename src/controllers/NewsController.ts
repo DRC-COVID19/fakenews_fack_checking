32df65133f28f538a2070fb095d29b350ff429d9
Christian Lisangola <christian.lisangola@gmail.com>
2020-04-02 14:00:43 +0100

    refactor status button color condition

--- src/controllers/NewsController.ts
+++ src/controllers/NewsController.ts
@@ -3,7 +3,7 @@ import { Information } from '../models/Information';
 export class NewsController {
   static home() {
     return async function(req: Request, res: Response) {
-      const news = await Information.find({ veracite: 'fausse' }).select(
+      const news = await Information.find().select(
         '_id source titre contenu photo veracite'
       );
       console.log('DATA : ', news);
