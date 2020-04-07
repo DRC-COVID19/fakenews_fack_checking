e30078b499ad26f537a582dec3186f5ce4cc20b8
OseeMukaya <oseemukaya@gmail.com>
2020-04-08 00:07:03 +0200

    validation form terminée

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,7 +1,8 @@
 import { Request, Response} from 'express';
 import mongoose from 'mongoose';
 import { User } from '../models/User';
-const Joi: any = require('joi');
+const { check, validationResult } = require('express-validator');
+const Joi = require('joi');
 
 export class UserController{
     static User(){
@@ -11,35 +12,28 @@ export class UserController{
     }
     static InsertUser(){
         return function(req:Request, res:Response){
-            const schema: any = Joi.object().keys({
-                userName: Joi.string().trim().requered(),
-                userPrenom: Joi.string().trim().requered(),
-                userPseudo: Joi.string().trim().requered(),
-                userEmail: Joi.string().trim().email().requered()
-            });
-            Joi.validate(req.body, schema,(error: any, result: any)=>{
-                if(error){
-                    console.log("Erreur")
-                }else{
-                    const user = new User({
-                        _id: new mongoose.Types.ObjectId(),
-                        nom: req.body.userName,
-                        prenom: req.body.userPrenom,
-                        pseudo: req.body.userPseudo,
-                        email: req.body.userEmail,
-                        password: req.body.userPassword
-                    });
-                    user.save()
-                    .then((result)=>{
-                        if(result){
-                            return res.redirect('/details/user'); 
-                        }
-                    })
-                    .catch((error)=>{
-                        console.log(error);
-                    });
-                }
-            });
+            const errors = validationResult(req);
+            if(!errors.isEmpty()){
+                res.redirect('/add/user');
+            }else{
+                const user = new User({
+                    _id: new mongoose.Types.ObjectId(),
+                    nom: req.body.userName,
+                    prenom: req.body.userPrenom,
+                    pseudo: req.body.userPseudo,
+                    email: req.body.userEmail,
+                    password: req.body.userPassword
+                });
+                user.save()
+                .then((result)=>{
+                    if(result){
+                        return res.redirect('/details/user'); 
+                    }
+                })
+                .catch((error)=>{
+                    console.log(error);
+                });
+            }
         }
     }
     static userDetails(){
@@ -83,23 +77,29 @@ export class UserController{
     }
     static editUser(){
         return function(req:Request, res:Response){
-            let user: any = {};
-            user.nom =  req.body.userName;
-            user.prenom = req.body.userPrenom;
-            user.pseudo = req.body.userPseudo;
-            user.email = req.body.userEmail;
-            user.password = req.body.userPassword;
+            const errors = validationResult(req);
+            if(!errors.isEmpty()){
+                res.redirect('/edit/user');
+            }else{
+                let user: any = {};
+                user.nom =  req.body.userName;
+                user.prenom = req.body.userPrenom;
+                user.pseudo = req.body.userPseudo;
+                user.email = req.body.userEmail;
+                user.password = req.body.userPassword;
 
-            let query = {_id: req.body._id};
-           if(req.body._id){
-                User.update(query,user,(error)=>{
-                    if(error){
-                        console.log(error);
-                    }else{
-                        res.redirect('/details/user');
-                    }
-                });
-           }
+                let query = {_id: req.body._id};
+            if(req.body._id){
+                    User.update(query,user,(error)=>{
+                        if(error){
+                            console.log(error);
+                        }else{
+                            res.redirect('/details/user');
+                        }
+                    });
+            }
+            }
+            
         }
     }
 }
--- src/routes/users_routes.ts
+++ src/routes/users_routes.ts
@@ -4,8 +4,18 @@ export const userRouter = AppRouter.getInstance();
 const { check, validationResult } = require('express-validator');
 
 userRouter.get('/add/user', UserController.User());
-userRouter.post('/add/user',UserController.InsertUser());
+userRouter.post('/add/user',
+[check('userEmail').not().isEmpty(),
+check('userPrenom').not().isEmpty(),
+check('userName').not().isEmpty(),
+check('userPseudo').not().isEmpty(),
+check('userPassword').not().isEmpty()],UserController.InsertUser());
 userRouter.get('/details/user', UserController.userDetails());
 userRouter.get('/details/user/:userId', UserController.userDelete());
 userRouter.get('/edit/user/:userId', UserController.findUser());
-userRouter.post('/edit/user', UserController.editUser());
+userRouter.post('/edit/user',
+[check('userEmail').not().isEmpty(),
+check('userPrenom').not().isEmpty(),
+check('userName').not().isEmpty(),
+check('userPseudo').not().isEmpty(),
+check('userPassword').not().isEmpty()],UserController.editUser());
