19e9fe29c04cf0bbbe49b1ba6e91173cce50ff18
Christian Lisangola <christian.lisangola@gmail.com>
2020-04-10 07:55:02 +0100

    Search result count

--- public/js/search-news.js
+++ public/js/search-news.js
@@ -2,15 +2,27 @@ const searchButton = document.querySelector('#search-btn');
 
 searchButton.addEventListener('click', (e) => {
   e.preventDefault();
-  const newsCards = this.document.querySelector('#loadedNews');
+  const searchTerm = document.querySelector('#keyWord').value;
+  if(!searchTerm.length){
+      return;
+  }
+  const newsCards = document.querySelector('#loadedNews');
+  const newsTitlesZone=document.querySelector('.row.mt-4.mb-4');
+  if(newsTitlesZone.children.length===3){
+      newsTitlesZone.removeChild(newsTitlesZone.lastElementChild);
+  }
   newsCards.innerHTML = '';
   generateLoaders(cardLoaderTemplate, 6, newsCards);
-  const searchTerm = document.querySelector('#keyWord').value;
+  
   axios
     .get(`http://localhost:3000/news/search?keyword=${searchTerm}`)
     .then((res) => {
       newsCards.innerHTML = '';
-
+    newsTitlesZone.insertAdjacentHTML('beforeend',`<div class="col-sm-12" style="text-align:start">
+        <h4 class="alert alert-light" role="alert">
+            ${res.data.news.length} résultats
+       </h4>
+    </div>  `);
        
     //   if(res.data.news.lenfth){
          loadNews(res.data.news, newsCards);
