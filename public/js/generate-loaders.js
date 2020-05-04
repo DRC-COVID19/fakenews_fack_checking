6fe5b214e216dc443aa24c427bcbaf7c82756895
Jean-Louis <jeanlouis.mbaka@gmail.com>
2020-05-04 23:58:14 +0100

    Removes unused files and optimize images

--- public/js/generate-loaders.js
+++
@@ -1,6 +0,0 @@
-const generateLoaders = (htmlString, numberOfOccurences, parentElement) => {
-  for (let occurence = 1; occurence <= numberOfOccurences; occurence++) {
-    parentElement.insertAdjacentHTML('afterbegin', htmlString);
-  }
-  return;
-};
